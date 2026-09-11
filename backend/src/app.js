2f50a5c63811fa11c6971867e70293ccef5e7482
Sehajpreet Kaur <sehajpreetk2967@gmail.com>
2026-09-11 16:15:23 +0530

    CORS updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,9 @@ import submissionRoutes from "./routes/submissions.js";
 
 export function createApp() {
   const app = express();
-  app.use(cors());
+  app.use(cors({
+    origin : process.env.FRONTEND_URL
+}));
   app.use(express.json());
 
   app.get("/health", (req, res) => res.json({ ok: true }));
